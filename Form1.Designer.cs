2a389a4c4b504fcb382cb32c4411baa2978d4001
byalcin <byalcin@isbak.istanbul>
2023-10-09 13:39:09 +0300

    #2_Fonksiyonlar-ve-Calisma-Mantigi-Olusturuldu

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -29,6 +29,15 @@ namespace RandomnessCoinTossApp
         /// </summary>
         private void InitializeComponent()
         {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea2 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend2 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series2 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea3 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend3 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series3 = new System.Windows.Forms.DataVisualization.Charting.Series();
             this.percentNumUpDown = new System.Windows.Forms.NumericUpDown();
             this.percentLabel = new System.Windows.Forms.Label();
             this.calculateButton = new System.Windows.Forms.Button();
@@ -38,10 +47,16 @@ namespace RandomnessCoinTossApp
             this.numericUpDownTryCount1 = new System.Windows.Forms.NumericUpDown();
             this.numericUpDownTryCount2 = new System.Windows.Forms.NumericUpDown();
             this.numericUpDownTryCount3 = new System.Windows.Forms.NumericUpDown();
+            this.graphOfTryCount3 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.graphOfTryCount2 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.graphOfTryCount1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
             ((System.ComponentModel.ISupportInitialize)(this.percentNumUpDown)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.numericUpDownTryCount1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.numericUpDownTryCount2)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.numericUpDownTryCount3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.graphOfTryCount3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.graphOfTryCount2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.graphOfTryCount1)).BeginInit();
             this.SuspendLayout();
             // 
             // percentNumUpDown
@@ -63,12 +78,13 @@ namespace RandomnessCoinTossApp
             // 
             // calculateButton
             // 
-            this.calculateButton.Location = new System.Drawing.Point(13, 140);
+            this.calculateButton.Location = new System.Drawing.Point(294, 87);
             this.calculateButton.Name = "calculateButton";
             this.calculateButton.Size = new System.Drawing.Size(124, 33);
             this.calculateButton.TabIndex = 2;
             this.calculateButton.Text = "Hesapla";
             this.calculateButton.UseVisualStyleBackColor = true;
+            this.calculateButton.Click += new System.EventHandler(this.CalculateButton_Click);
             // 
             // labelTryNumber1
             // 
@@ -158,11 +174,62 @@ namespace RandomnessCoinTossApp
             0,
             0});
             // 
+            // graphOfTryCount3
+            // 
+            chartArea1.Name = "ChartArea1";
+            this.graphOfTryCount3.ChartAreas.Add(chartArea1);
+            legend1.Name = "Legend1";
+            this.graphOfTryCount3.Legends.Add(legend1);
+            this.graphOfTryCount3.Location = new System.Drawing.Point(36, 426);
+            this.graphOfTryCount3.Name = "graphOfTryCount3";
+            series1.ChartArea = "ChartArea1";
+            series1.Legend = "Legend1";
+            series1.Name = "Series1";
+            this.graphOfTryCount3.Series.Add(series1);
+            this.graphOfTryCount3.Size = new System.Drawing.Size(764, 127);
+            this.graphOfTryCount3.TabIndex = 16;
+            this.graphOfTryCount3.Text = "chart1";
+            // 
+            // graphOfTryCount2
+            // 
+            chartArea2.Name = "ChartArea1";
+            this.graphOfTryCount2.ChartAreas.Add(chartArea2);
+            legend2.Name = "Legend1";
+            this.graphOfTryCount2.Legends.Add(legend2);
+            this.graphOfTryCount2.Location = new System.Drawing.Point(36, 282);
+            this.graphOfTryCount2.Name = "graphOfTryCount2";
+            series2.ChartArea = "ChartArea1";
+            series2.Legend = "Legend1";
+            series2.Name = "Series1";
+            this.graphOfTryCount2.Series.Add(series2);
+            this.graphOfTryCount2.Size = new System.Drawing.Size(764, 127);
+            this.graphOfTryCount2.TabIndex = 17;
+            this.graphOfTryCount2.Text = "chart1";
+            // 
+            // graphOfTryCount1
+            // 
+            chartArea3.Name = "ChartArea1";
+            this.graphOfTryCount1.ChartAreas.Add(chartArea3);
+            legend3.Name = "Legend1";
+            this.graphOfTryCount1.Legends.Add(legend3);
+            this.graphOfTryCount1.Location = new System.Drawing.Point(36, 140);
+            this.graphOfTryCount1.Name = "graphOfTryCount1";
+            series3.ChartArea = "ChartArea1";
+            series3.Legend = "Legend1";
+            series3.Name = "Series1";
+            this.graphOfTryCount1.Series.Add(series3);
+            this.graphOfTryCount1.Size = new System.Drawing.Size(764, 127);
+            this.graphOfTryCount1.TabIndex = 18;
+            this.graphOfTryCount1.Text = "chart1";
+            // 
             // MainForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(861, 565);
+            this.Controls.Add(this.graphOfTryCount1);
+            this.Controls.Add(this.graphOfTryCount2);
+            this.Controls.Add(this.graphOfTryCount3);
             this.Controls.Add(this.numericUpDownTryCount3);
             this.Controls.Add(this.numericUpDownTryCount2);
             this.Controls.Add(this.numericUpDownTryCount1);
@@ -179,6 +246,9 @@ namespace RandomnessCoinTossApp
             ((System.ComponentModel.ISupportInitialize)(this.numericUpDownTryCount1)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.numericUpDownTryCount2)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.numericUpDownTryCount3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.graphOfTryCount3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.graphOfTryCount2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.graphOfTryCount1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -195,6 +265,9 @@ namespace RandomnessCoinTossApp
         private System.Windows.Forms.NumericUpDown numericUpDownTryCount1;
         private System.Windows.Forms.NumericUpDown numericUpDownTryCount2;
         private System.Windows.Forms.NumericUpDown numericUpDownTryCount3;
+        private System.Windows.Forms.DataVisualization.Charting.Chart graphOfTryCount3;
+        private System.Windows.Forms.DataVisualization.Charting.Chart graphOfTryCount2;
+        private System.Windows.Forms.DataVisualization.Charting.Chart graphOfTryCount1;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -1,20 +1,53 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace RandomnessCoinTossApp
 {
     public partial class MainForm : Form
     {
+        Random random = new Random();
+
+        NumericUpDown[] AttemptsList;
+
+        int heads, tails;
+
         public MainForm()
         {
             InitializeComponent();
+        }
+
+        private void CalculateHeadOrTails(int attempts, int invalid)
+        {
+            heads = 0; tails = 0; //Heads : Yazı , Tails ; Tura.
+
+            attempts *= (1 - (invalid / 100));
+
+            for (int i = 0; i < attempts; i++)
+            {
+                int rand = random.Next();
+
+                if (rand % 2 == 0)
+                    heads++;
+                else
+                    tails++;
+            }
+        }
+
+        private void CalculateButton_Click(object sender, EventArgs e)
+        {
+            AttemptsList = new NumericUpDown[] { numericUpDownTryCount1, numericUpDownTryCount2, numericUpDownTryCount3 };
+
+            try
+            {
+                for (int i = 0; i < AttemptsList.Length; i++)
+                {
+                    CalculateHeadOrTails((int)AttemptsList[i].Value, (int)percentNumUpDown.Value);
+                }
+            }
+            catch (Exception ex)
+            {
+
+            }
         }
     }
 }
